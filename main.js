f180c6ed6e99447ee25520201f53da4feac23fc3
Aaron Dean <adean30462@gmail.com>
2023-02-15 14:03:42 -0500

    conditional work

---
+++ main.js
@@ -0,0 +1,83 @@
+//----------examples---------//
+// if(2+2 === 4){
+//     console.log("Math works!");
+//   } else {
+//     console.log("Math is broken.")
+//   }
+  
+//   // Expected output: "Math works!"
+
+// var time ="afternoon";
+
+// if (time === "morning"){
+//   console.log("Good morning!");
+// } else if (time === "afternoon"){
+//   console.log("Good afternoon!");
+// } else if (time === "evening"){
+//   console.log("Good evening!");
+// } else {
+//   console.log("Hello!");
+// }
+
+// // Expected output: "Good afternoon!"
+
+if(1+1 > 5){
+    console.log("True fact!");
+  } else {
+    console.log("LIES!")
+  };
+  
+  if(4+5 <= 20){
+    console.log("True fact!");
+  } else {
+    console.log("LIES!")
+  };
+
+  if(6-2 >= 0){
+    console.log("True fact!");
+  } else {
+    console.log("LIES!")
+  };
+
+var likesDogs = true;
+
+if (likesDogs){
+    console.log("You're a dog person!")} else if (console.log("You're a cat person!"));
+
+    // object ex. 
+    //const desk = {
+    //     height: 42,
+    //     width: 63,
+    //     depth: 30
+    // }
+
+    const person = {
+        name: "Aaron",
+        language: "English"
+    }
+
+
+if (person.language === "English") {
+    console.log(`Hello, ${person.name}!`)
+} else if (person.language === "Spanish")
+   { console.log(`Hola, ${person.name}`)
+} else if (person.language === "French") {
+    console.log(`Bonjour, ${person.name}`)
+};
+
+let grade = 93;
+ if(grade <= 69 && grade > 0) {
+    console.log("You got an F") }
+ else if(grade >= 70 && grade < 76) {
+    console.log("You got a D")
+ }
+ else if(grade >= 77 && grade < 84) {
+    console.log("You got a C")
+ }
+ else if(grade >= 84 && grade < 92) {
+    console.log("You got a B")
+ }
+ else if(grade >= 92 && grade < 100) {
+    console.log("You got an A")
+ }
+ 
